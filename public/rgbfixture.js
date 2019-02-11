777bf7bbeae1e84d66a8aa3098cef23caf4140a9
shadowfax6894 <anniekelly6894@gmail.com>
2019-02-10 17:14:58 -0700

    better channel picking mecahnism

--- public/rgbfixture.js
+++ public/rgbfixture.js
@@ -19,26 +19,21 @@ class RGBFixture extends Fixture {
         this.brightnesspicker = createSlider(0, 255, this.brightness);
         this.brightnesspicker.position(this.x/4, this.y+this.size+50);
         this.brightnesspicker.changed(() => this.updateBrightness(this.brightnesspicker.value()));    
-        this.redChannelInput = createInput();
+        this.redChannelInput = createSelect();
         this.redChannelInput.position(this.x-this.size/4+65, this.y+this.size+44);
-        this.redChannelInput.changed(() => this.updateRedChannelNumber(this.brightnesspicker.value()));       
-        this.redChannelInput.size(15);                                                   
-        this.greenChannelInput = createInput();
+        this.redChannelInput.changed(() => this.updateRedChannelNumber(this.redChannelInput.value()));    
+        this.greenChannelInput = createSelect();
         this.greenChannelInput.position(this.x-this.size/4+65, this.y+this.size+64);
-        this.greenChannelInput.changed(() => this.updateGreenChannelNumber(this.brightnesspicker.value()));    
-        this.greenChannelInput.size(15);                                                   
-        this.blueChannelInput = createInput();
+        this.greenChannelInput.changed(() => this.updateGreenChannelNumber(this.greenChannelInput.value()));    
+        this.blueChannelInput = createSelect();
         this.blueChannelInput.position(this.x-this.size/4+65, this.y+this.size+84);
-        this.blueChannelInput.changed(() => this.updateBlueChannelNumber(this.brightnesspicker.value()));     
-        this.blueChannelInput.size(15);                                                   
-        this.whiteChannelInput = createInput();
+        this.blueChannelInput.changed(() => this.updateBlueChannelNumber(this.blueChannelInput.value()));     
+        this.whiteChannelInput = createSelect();
         this.whiteChannelInput.position(this.x-this.size/4+65, this.y+this.size+104);
-        this.whiteChannelInput.changed(() => this.updateWhiteChannelNumber(this.brightnesspicker.value()));   
-        this.whiteChannelInput.size(15);                                                   
-        this.brightnessChannelInput = createInput();
+        this.whiteChannelInput.changed(() => this.updateWhiteChannelNumber(this.whiteChannelInput.value()));   
+        this.brightnessChannelInput = createSelect();
         this.brightnessChannelInput.position(this.x-this.size/4+65, this.y+this.size+124);
-        this.brightnessChannelInput.changed(() => this.updateBrightnessChannelNumber(this.brightnesspicker.value()));    
-        this.brightnessChannelInput.size(15);                                                   
+        this.brightnessChannelInput.changed(() => this.updateBrightnessChannelNumber(this.brightnessChannelInput.value()));    
         /*this.channels[this.brightnessChannel].value = 255;
         this.channels[this.redChannel].value = 255;
         this.channels[this.greenChannel].value = 0;
@@ -51,21 +46,22 @@ class RGBFixture extends Fixture {
         this.r = rgbColor['r'];
         this.g = rgbColor['g'];
         this.b = rgbColor['b'];
-        if (this.channels[this.redChannel]) {
-            this.channels[this.redChannel].value = this.r;
+        console.log(this.channels);
+        if (this.channels[this.redChannel-1]) {
+            this.channels[this.redChannel-1].value = this.r;
         }
-        if (this.channels[this.greenChannel]) {
-            this.channels[this.greenChannel].value = this.g;
+        if (this.channels[this.greenChannel-1]) {
+            this.channels[this.greenChannel-1].value = this.g;
         }
-        if (this.channels[this.blueChannel]) {
-            this.channels[this.blueChannel].value = this.b;
+        if (this.channels[this.blueChannel-1]) {
+            this.channels[this.blueChannel-1].value = this.b;
         }
       }
     
       updateBrightness() { 
           this.brightness = this.brightnesspicker.value(); 
-            if (this.channels[this.brightnessChannel]) {
-                this.channels[this.brightnessChannel].value = this.brightness;
+            if (this.channels[this.brightnessChannel-1]) {
+                this.channels[this.brightnessChannel-1].value = this.brightness;
             }
         }
 
@@ -102,33 +98,58 @@ class RGBFixture extends Fixture {
           }
 
           updateBrightnessChannelNumber(channelNumber) {
+            this.channels[this.brightnessChannel - 1].value = 0;
             this.brightnessChannel = channelNumber;
             this.updateColor();
             this.updateBrightness();
           }
 
           updateRedChannelNumber(channelNumber) {
+            this.channels[this.redChannel - 1].value = 0;
             this.redChannel = channelNumber;
             this.updateColor();
             this.updateBrightness();
           }        
 
           updateGreenChannelNumber(channelNumber) {
+            this.channels[this.greenChannel - 1].value = 0;
             this.greenChannel = channelNumber;
             this.updateColor();
             this.updateBrightness();
           }
 
           updateBlueChannelNumber(channelNumber) {
+            this.channels[this.blueChannel - 1].value = 0;
             this.blueChannel = channelNumber;
             this.updateColor();
             this.updateBrightness();
           }
            
           updateWhiteChannelNumber(channelNumber) {
+            this.channels[this.whiteChannel - 1].value = 0;
             this.whiteChannel = channelNumber;
             this.updateColor();
             this.updateBrightness();
-          }          
+          } 
+          
+          updateChannelNumber() { 
+            this.numChannels = this.channelsInput.value();
+            this.channels = [];
+            this.redChannelInput['elt'].innerHTML = '';
+            console.log(this.redChannelInput['elt']);
+            for (var i = 0; i < this.numChannels; i++) {
+              this.channels.push(new Channel(i+1));
+              this.redChannelInput.option(i+1);
+              this.greenChannelInput.option(i+1);
+              this.blueChannelInput.option(i+1);
+              this.whiteChannelInput.option(i+1);
+              this.brightnessChannelInput.option(i+1);
+            }
+              this.redChannel = 1;
+              this.greenChannel = 1;
+              this.blueChannel = 1;
+              this.whiteChannel = 1;
+              this.brightnessChannel = 1;
+            }            
 
 }
