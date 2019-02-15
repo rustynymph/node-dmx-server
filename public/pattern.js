2d6c19bb7b696923167cd47eb127f410332663eb
shadowfax6894 <anniekelly6894@gmail.com>
2019-02-15 12:15:30 -0700

    added alert messages for animation editor

--- public/pattern.js
+++ public/pattern.js
@@ -4,6 +4,7 @@ class Pattern {
       this.scenes = [];
       this.sceneButtons = [];
       this.timeInputs = [];
+      this.deleteSceneButtons = [];
       this.isLooping = false;
       this.timeIntervalID = null;
       this.timeOuts = [];
@@ -11,6 +12,11 @@ class Pattern {
 
   /* to do: need to check and make sure rig is consistently connected and set up */
   saveScene() {
+    if (!universe.dmxController.out.connectedTo) {
+      alert("You must have at least one connected in your fixture to save a scene");
+      return;
+    }
+
     var scene = new Scene(this.scenes.length);
     for (var f = 0; f < universe.fixtures.length; f++) {
       var fixture = universe.fixtures[f];
@@ -26,9 +32,30 @@ class Pattern {
     }
     this.scenes.push(scene);
     this.addSceneButton(scene);
+    this.addRemoveSceneButton(scene);
     this.addSceneTimeInput(scene);
   }
     
+  removeScene(scene) {
+    if (confirm("Are you sure you want to delete scene #" + parseInt(scene.number+1) + "?")) {
+      // delete 
+      console.log("deleting shit");
+      for (var s = 0; s < this.scenes.length; s++) {
+        if (scene.number = s) {
+          this.scenes.splice(s);
+          this.sceneButtons[s].remove();
+          this.timeInputs[s].remove();
+          this.deleteSceneButtons[s].remove();
+          this.sceneButtons.splice(s);
+          this.timeInputs.splice(s);
+          this.deleteSceneButtons.splice(s);
+        }
+      }
+    } else {
+      // do nothing
+    }   
+  }
+
   addSceneButton(scene) {
     var btn = createButton('Scene ' + (scene.number+1).toString());
     btn.mousePressed(() => scene.show());
@@ -40,11 +67,20 @@ class Pattern {
 
   addSceneTimeInput(scene) {
     var timeInput = createInput('500');
+    timeInput.size(50);
     timeInput.changed(() => {scene.length = parseInt(timeInput.value())});
     this.timeInputs.push(timeInput);
     var msTextNode = document.createTextNode("ms");
     scenesElement['elt'].appendChild(timeInput['elt']);   
     scenesElement['elt'].appendChild(msTextNode);     
+  }
+
+  addRemoveSceneButton(scene) {
+    var deletebtn = createButton('X');
+    deletebtn['elt'].style.color = 'red';
+    deletebtn.mousePressed(() => this.removeScene(scene));
+    scenesElement['elt'].appendChild(deletebtn['elt']);  
+    this.deleteSceneButtons.push(deletebtn); 
   }
 
   play() {
